7d8241dc4056c65bd8b3272966c16bbf437be678
TharinduCK <tharinduwijayarathne87@gmail.com>
2022-07-25 06:56:58 +0530

    Refund process main part is done and report generating part is still remains

---
+++ src/gui/Invoice_item_selector.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import com.formdev.flatlaf.intellijthemes.FlatLightFlatIJTheme;
+import java.sql.ResultSet;
+import java.util.Vector;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import model.MySQL;
+
+/**
+ *
+ * @author thari
+ */
+public class Invoice_item_selector extends javax.swing.JDialog {
+
+    Refund_panel rp;
+
+    public Invoice_item_selector(java.awt.Dialog parent, boolean modal) {
+        super(parent, modal);
+        this.rp = (Refund_panel) parent;
+        initComponents();
+        load();
+    }
+
+    private void load() {
+        String uid = rp.jTextField1.getText();
+
+        try {
+
+            ResultSet rs = MySQL.search("SELECT * FROM `invoice` INNER JOIN `invoice_item` ON `invoice_item`.`invoice_id` = `invoice`.`id` INNER JOIN `stock` ON `stock`.`id` = `invoice_item`.`stock_id` INNER JOIN `product` ON `stock`.`product_id` = `product`.`id` WHERE `invoice`.`unique_id` = '" + uid + "' ORDER BY `invoice`.`id` ASC");
+
+            DefaultTableModel dtm = (DefaultTableModel) jTable1.getModel();
+            dtm.setRowCount(0);
+
+            while (rs.next()) {
+                Vector v = new Vector();
+                v.add(rs.getString("product.id"));
+                v.add(rs.getString("product.name"));
+                v.add(rs.getString("invoice.user_id"));
+                v.add(rs.getString("invoice.id"));
+                v.add(rs.getString("stock.id"));
+                v.add(rs.getString("stock.selling_price"));
+
+                dtm.addRow(v);
+            }
+
+            jTable1.setModel(dtm);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Invoice Details Selector");
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Product ID", "Product Name", "Cashier ID", "Invoice ID", "Stock ID", "Selling Price"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 601, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        if (evt.getClickCount() == 2) {
+            int r = jTable1.getSelectedRow();
+
+            if (r == -1) {
+                JOptionPane.showMessageDialog(this, "Please select a product", "Warning", JOptionPane.WARNING_MESSAGE);
+            } else {
+
+                String pid = jTable1.getValueAt(r, 0).toString();
+                String pname = jTable1.getValueAt(r, 1).toString();
+                String cid = jTable1.getValueAt(r, 2).toString();
+                String iid = jTable1.getValueAt(r, 3).toString();
+                String sid = jTable1.getValueAt(r, 4).toString();
+                String sp = jTable1.getValueAt(r, 5).toString();
+
+                rp.jTextField2.setText(pname);
+                rp.jLabel14.setText(sp);
+                rp.jLabel6.setText(cid);
+                rp.jLabel7.setText(iid);
+                rp.jLabel10.setText(sid);
+                rp.jLabel11.setText(pid);
+
+                this.dispose();
+
+            }
+        }
+    }//GEN-LAST:event_jTable1MouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        FlatLightFlatIJTheme.setup();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Invoice_item_selector dialog = new Invoice_item_selector(new javax.swing.JDialog(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration//GEN-END:variables
+}
